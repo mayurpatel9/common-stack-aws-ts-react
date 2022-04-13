3c162245e710262e9e641488de4d339ea67e1823
Mayur Patel <Mayur.Patel@ama.ab.ca>
2022-04-13 17:11:14 -0600

    added lambda, apigateway and dynamodb

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,35 +1,30 @@
 import { Stack, StackProps, CfnParameter, Duration, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Bucket } from 'aws-cdk-lib/aws-s3'
+import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
+import {LambdaIntegration, RestApi} from 'aws-cdk-lib/aws-apigateway'
+import { HttpMethod } from 'aws-cdk-lib/aws-events';
+import { GenericTable } from './GenericTable';
 
 export class CdkStack extends Stack {
+  
+  private api = new RestApi(this, 'helloApi')
+  private spaceTable = new GenericTable('SpacesTable', 'spaceId', this)
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const days = new CfnParameter(this, 'duration', {
-      type: 'Number',
-      default: 6,
-      minValue: 1,
-      maxValue: 10
 
+
+    const helloLambda = new LambdaFunction(this, 'helloFunction', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+      handler: 'hello.main'
     })
 
-    const myBucket = new Bucket(this, 'myBucket', {
-      lifecycleRules: [{
-        expiration: Duration.days(days.valueAsNumber)
-      }]
-    })
-
-    new CfnOutput(this, 'outputBucket', {
-      value: myBucket.bucketName
-    });
- 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //hello api integration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource('hello');
+    helloLambdaResource.addMethod(HttpMethod.GET, helloLambdaIntegration);
   }
 }

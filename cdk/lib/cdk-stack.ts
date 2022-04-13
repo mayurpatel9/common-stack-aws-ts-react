4ac1dda80cd93a700d272ac6bfe31d5e2052547e
Mayur Patel <Mayur.Patel@ama.ab.ca>
2022-04-12 22:30:00 -0600

    updated stack

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,12 +1,29 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnParameter, Duration, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Bucket } from '@aws-cdk/aws-s3'
+import { Bucket } from 'aws-cdk-lib/aws-s3'
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const duration = new CfnParameter(this, 'duration', {
+      type: 'Number',
+      default: 6,
+      minValue: 1,
+      maxValue: 10
+
+    })
+
+    const myBucket = new Bucket(this, 'bucket', {
+      lifecycleRules: [{
+        expiration: Duration.days(duration.valueAsNumber)
+      }]
+    })
+
+    new CfnOutput(this, 'bucket', {
+      value: myBucket.bucketName
+    });
  
     // The code that defines your stack goes here
 

54340e1bf2196a2684d74ca3336d2e14bebbdc14
Mayur Patel <Mayur.Patel@ama.ab.ca>
2022-04-12 21:51:07 -0600

    initial commit

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,18 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket } from '@aws-cdk/aws-s3'
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+ 
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
